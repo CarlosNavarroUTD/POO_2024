2770e5c0d0a7d10b9bd182ce24ea238fdaad054a
CarlosNavarroUTD <167482783+CarlosNavarroUTD@users.noreply.github.com>
2024-05-18 16:53:11 -0600

    Subí todos los documentos

---
+++ 2_variables_tipos_datos/Tiposdatos.py
@@ -0,0 +1,51 @@
+#los tipos de datos mas comunes en python son:
+
+#simples o primitivos
+#int (entero)
+#float (real)
+#bool(logico)
+
+#estructurados
+#str (cadena)
+#list(lista)
+#tuple
+#dict(como un objeto)
+
+entero=23
+float=3.1416
+logico=False
+
+#estructurados
+palabra="hola"
+palabra2="@"
+lista=[10,20,30,40]
+lista2=(True,10,3.3,"Que tal","9")
+tuple=("Hola","que","tal")
+dict={
+    "nombre":"Carlos",
+    "apellido":"Navarro",
+    "edad":20,
+    "estatura":1.7
+}
+
+#Mostrar el tipo de dato de cada identificador
+print(type(entero))
+print(type(float))
+print(type(logico))
+print(type(palabra))
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
