ada759aa2dc2903f1d79457c6d1d6588d7a95f4c
CarlosNavarroUTD <carlos_3041230362@utd.edu.mx>
2024-06-07 10:36:13 -0600

    tuplas, sets y diccinarios

--- examen/practica_funciones.py
+++ examen/practica_funciones.py
@@ -1,5 +1,5 @@
 import os
-movies=[]
+movies=["movie1"]
 def esperarTecla():
   print("Oprima cualquier tecla para continuar ...")
   input()
@@ -18,19 +18,37 @@ def readMovie(movie):
 
 
 
-print(movies)
+
 
 i=True
 while i:
-    os.system("Clear")
+    os.system("clear")
     print(f"========== \n WachaMovies \n=============")
     print("1.- Añadir")
     print("2.- Remover")
-    print("3.- Consultar")
+    print("3.- Buscar")
+    print("4.- Listar")
     print("5.- Salir")
     opcion= input("Elige una opción ").upper()
 
     if opcion=="1" or opcion== "añadir":
+        movie=input("Nombre de pelicula: ")
         addMovie(movie)
-movie=input("Nombre de pelicula: ")
+    
+    if opcion=="2" or opcion== "remover":
+        movie=input("Pelicula a borrar: ")
+        if movie in movies:
+            delMovie(movie)
+            print("Pelicula Borrada")
+            esperarTecla()
+        else:
+            print("Pelicula no encontrada")
+            esperarTecla()
 
+    if opcion=="3" or opcion== "Buscar":
+        movie=input("Pelicula a buscar: ")
+        readMovie(movie)
+        esperarTecla()
+
+print(movies)
+esperarTecla()
