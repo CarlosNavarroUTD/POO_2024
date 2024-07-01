3e08db902ab213940f5989c257ca28ab7c48cec5
CarlosNavarroUTD <167482783+CarlosNavarroUTD@users.noreply.github.com>
2024-07-01 12:00:29 -0600

    Delete parcial2_2B directory

--- parcial2_2B/7_proyectos/figuras/objetos.py
+++
@@ -1,9 +0,0 @@
-from clases import *
-
-#Rectangulos
-figura1 = Rectangulos(3, 4, True, 10, 20)
-print(figura1.calcularArea())
-
-#Circulos
-circulo1 = Circulos(3, 3, True, 6)
-print(circulo1.calcularArea())
--- parcial2_2B/7_proyectos/objetos.py
+++
@@ -1,8 +0,0 @@
-from clases import *
-figura1=rectangulos(3,4,True, 10, 20)
-
-figura1.calcularArea(10, 20)
-
-
-circulo1=circulos(3,3,True, 6)
-circulo1.calcularArea(1,2,6)
